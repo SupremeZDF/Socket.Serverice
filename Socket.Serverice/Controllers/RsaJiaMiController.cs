0e71453a864d193211b81847b7819f8da310dae3
东风 赵 <zdf@cheok.com>
2020-05-06 10:35:23 +0800

    代码提交

---
+++ Socket.Serverice/Controllers/RsaJiaMiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Socket.Serverice.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RsaJiaMiController : ControllerBase
+    {
+
+    }
+}
